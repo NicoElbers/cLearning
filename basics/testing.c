6a7fd5465a7fe759dfb48bdf31f044b718836697
Nico Elbers <nico.b.elbers@gmail.com>
2023-09-21 10:47:04 +0200

    fucked git gitignore, made a compile script and did testing on arrays

---
+++ basics/testing.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void addArr(int[], int);
+
+int main(int argc, char *argv[]) {
+  int nums[] = {1, 2, 3, 4, 54};
+  int ARRLEN = 10;
+  int nums2[] = {4, 4, 8, 5, 2, 3, 1, 1, 1, 62};
+
+  addArr(nums2, ARRLEN);
+  for (int i = 0; i < ARRLEN; ++i) {
+    printf("%i\n", nums2[i]);
+  }
+
+  return 0;
+}
+
+void addArr(int nums[], int ARRLEN) {
+  for (int i = 0; i < ARRLEN; ++i) {
+    nums[i] += 1;
+  }
+}
