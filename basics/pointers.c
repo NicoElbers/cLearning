27fba225ed35532685505e201dd0e1ad8efc92c7
NicoElbers <Ryojipog@gmail.com>
2023-09-25 17:03:38 +0200

    got the pointer thingy working

--- basics/pointers.c
+++ basics/pointers.c
@@ -1,24 +1,29 @@
 #include <stdio.h>
 
+void add(int *, int);
+
 int main() {
   int num[5] = {0, 1, 2, 3, 4};
   int *iptr = num;
 
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
-  printf("%d\n", *iptr);
-  iptr++;
+  add(iptr, 5);
 
-  char *cptr;
-  float *fptr;
+  printf("%d\n", *iptr);
+  iptr++;
+  printf("%d\n", *iptr);
+  iptr++;
+  printf("%d\n", *iptr);
+  iptr++;
+  printf("%d\n", *iptr);
+  iptr++;
+  printf("%d\n", *iptr);
+  iptr++;
+}
+
+void add(int *pntr, int n) {
+  for (int i = 0; i < n; ++i) {
+    *(pntr + i) += 2;
+    printf("%d\n", *(pntr + i));
+  }
+  printf("\n");
 }
